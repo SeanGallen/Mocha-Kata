d7f7d281cd1499cae46c19844e9f1b4290413923
Sean <sjgallen@gmail.com>
2016-10-01 18:43:47 -0500

    added a adding test to see how this works

--- spec/mathSpec.js
+++ spec/mathSpec.js
@@ -1,1 +1,11 @@
+var assert = require('assert');
 var math = require('../src/math.js');
+
+describe('Calculator', function() {
+  describe('adding', function() {
+    it('should be able to add', function() {
+      assert.equal(3, add(1,2));
+    });
+  })
+});
+
