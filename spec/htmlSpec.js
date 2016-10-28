b2b6fd68a0f4b1c5aee2d2d9049075c29886a136
Sean <sjgallen@gmail.com>
2016-10-27 22:11:53 -0500

    test button and used a Before hook for the test

--- spec/htmlSpec.js
+++ spec/htmlSpec.js
@@ -1,4 +1,6 @@
-describe('DOM', function() {
+describe('The Overall DOM', function() {
+  var myButton = document.getElementById('myBtn');
+
     it('The DOM lives', function() {
       expect(typeof document).to.equal( 'object');
     })
@@ -6,5 +8,25 @@ describe('DOM', function() {
      it('Title of the DOM', function() {
        expect(document.title).to.equal('Chia Test');
      })
+
+     it('It has a button object', function() {
+       expect(typeof myButton).to.equal('object');
+     })
+
+     it('The button has words', function() {
+       expect(myButton.innerHTML).to.equal('I am a button');
+     })
+
   });
 
+describe('Click is Responsive', function() {
+  var myButton = document.getElementById('myBtn');
+     before(function(done) {
+       myButton.click();
+       done();
+     });
+
+     it('The button does a trick', function() {
+       expect(myButton.innerHTML).to.equal('pressed');
+     })
+});
